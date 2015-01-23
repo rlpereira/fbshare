cba190c2931b9e145d3b9c6362a2094c0894f4e8
Pereira <rafaellimapereira@gmail.com>
2015-01-23 15:03:57 -0200

    dinamizando card

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ts=4 et
-import os
+import os, random
 from flask import Flask, request, Response, send_file, render_template
 
 app = Flask(__name__)
@@ -17,9 +17,12 @@ def share_img(cityid):
 
 @app.route("/")
 def index():
-    url = 'http://104.131.147.80:5050/share_img/363.png?temp=24'
+	temp = random.randint(0,42)
+	og_url = 'http://104.131.147.80:5050'
+	og_img = 'http://104.131.147.80:5050/share_img/363.png?temp=%d' % temp
+    fb_url = 'https://www.facebook.com/sharer/sharer.php?u=http://104.131.147.80:5050/?temp=%d' % temp
 
-    return render_template('index.html', og_url=url)
+    return render_template('index.html', og_url=og_url, fb_url=fb_url, og_img_url=og_img_url)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
