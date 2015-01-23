445d06cfb4d8eb768b009ba1d296f171287048ab
Pereira <rafaellimapereira@gmail.com>
2015-01-23 15:04:41 -0200

    corrigindo indentation

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def index():
 	temp = random.randint(0,42)
 	og_url = 'http://104.131.147.80:5050'
 	og_img = 'http://104.131.147.80:5050/share_img/363.png?temp=%d' % temp
-    fb_url = 'https://www.facebook.com/sharer/sharer.php?u=http://104.131.147.80:5050/?temp=%d' % temp
+	fb_url = 'https://www.facebook.com/sharer/sharer.php?u=http://104.131.147.80:5050/?temp=%d' % temp
 
-    return render_template('index.html', og_url=og_url, fb_url=fb_url, og_img_url=og_img_url)
+	return render_template('index.html', og_url=og_url, fb_url=fb_url, og_img_url=og_img_url)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
